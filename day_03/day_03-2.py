3fa7459ef680f224bec1848d7505d2fd8109c885
Boromir-the-Brave <zanegloh@gmail.com>
2022-12-09 18:24:59 -0800

    finished part 2

--- day_03/day_03-2.py
+++ day_03/day_03-2.py
@@ -14,7 +14,6 @@ def map_letter_prios():
 
 def main():
     map = map_letter_prios()
-    # print(map)
     file_name = "input.txt"
     lines = []
     total = 0
@@ -22,31 +21,35 @@ def main():
     with open(file_name, "r") as f:
         lines = f.readlines()
     
-    for i in range(len(lines)):
-        # line_len = 0
-        lines[i] = lines[i].rstrip()
+    for i in range(len(lines) // 3):
+        start_ind = i * 3
 
-        half_line_len = len(lines[i]) // 2
+        for j in range(3):
+            cur_ind = start_ind + j
 
-        # print(lines[i][0:half_line_len-1])
-        # print(lines[i][half_line_len:])
-        first_half_set = set(lines[i][0:half_line_len])
-        second_half_set = set(lines[i][half_line_len:])
+            lines[cur_ind] = lines[cur_ind].rstrip()
 
-        # print(first_half_set)
-        # print(second_half_set)
+        set_1 = set(lines[start_ind])
+        set_2 = set(lines[start_ind + 1])
+        set_3 = set(lines[start_ind + 2])
 
-        # print(first_half_set.intersection(second_half_set))
+        intersect = set_1.intersection(set_2.intersection(set_3))
 
-        intersect = first_half_set.intersection(second_half_set)
+        total += map[intersect.pop()]
 
-        if len(intersect) == 1:
-            total += map[intersect.pop()]
+
+
+        # half_line_len = len(lines[i]) // 2
+
+        # first_half_set = set(lines[i][0:half_line_len])
+        # second_half_set = set(lines[i][half_line_len:])
+
+        # intersect = first_half_set.intersection(second_half_set)
+
+        # if len(intersect) == 1:
+        #     total += map[intersect.pop()]
         
     print(total)
-
-
-
 
 
 
