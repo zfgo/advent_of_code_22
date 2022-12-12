854bea1be0342a1c72196bc88fefa8bd3738b5f9
Boromir-the-Brave <zanegloh@gmail.com>
2022-12-12 13:03:16 -0800

    finished part 2

---
+++ day_04/day_04-2.py
@@ -0,0 +1,38 @@
+import re
+
+def main():
+    file_name = "input.txt"
+    lines = []
+    ranges = []
+    total = 0
+
+    with open(file_name, 'r') as f:
+        lines = f.readlines()
+    
+    for i in range(len(lines)):
+        lines[i] = lines[i].rstrip()
+
+        tmp = []
+        tmp = re.split(',|-', lines[i])
+        # tmp = lines[i].split('-|,')
+        # print(tmp)
+        for j in range(4):
+            tmp[j] = int(tmp[j])
+        
+        ranges.append(tmp)
+    
+    for i in range(len(ranges)):
+        # possible valid outcomes:
+        if ranges[i][1] >= ranges[i][2] and ranges[i][1] <= ranges[i][3]:
+            total += 1
+            # print(ranges[i])
+        elif ranges[i][3] >= ranges[i][0] and ranges[i][3] <= ranges[i][1]:
+            total += 1
+            # print(ranges[i])
+
+    
+    print(total)
+
+
+if __name__ == "__main__":
+    main()
