a667283305aa2385a7beba12bf8331672faee58b
Boromir-the-Brave <zanegloh@gmail.com>
2022-12-15 17:56:03 -0800

    initial commit

---
+++ day_06/day_06-2.py
@@ -0,0 +1,12 @@
+def main():
+    file_name = "input.txt"
+    lines = []
+    
+
+    with open(file_name, 'r') as f:
+        lines = f.readlines()
+    
+    
+
+if __name__ == "__main__":
+    main()
