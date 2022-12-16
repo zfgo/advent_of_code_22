a8ba4b5f17107d16183ed0033342e4df0779b12b
Boromir-the-Brave <zanegloh@gmail.com>
2022-12-15 17:43:46 -0800

    finished part 1

--- day_05/day_05-1.py
+++ day_05/day_05-1.py
@@ -1,17 +1,36 @@
+columns = []
+
+
+def routine(amount_to_move, move_from, move_to):
+    move_from -= 1
+    move_to -= 1
+    # tmp = []
+
+    for _ in range(amount_to_move):
+        columns[move_to].append(columns[move_from].pop())
+    
+    # for _ in range(amount_to_move):
+        # columns[move_to].append(tmp.pop())
+
+    return
+
 
 def main():
     file_name = "input.txt"
     lines = []
     total = 0
     crate_ct = 0
+    row_ct = 0
     indicator = True
     column_ct = 0
+    output = ""
 
     with open(file_name, 'r') as f:
         lines = f.readlines()
     
     for i in range(len(lines)):
-        lines[i] = lines[i].rstrip()
+        lines[i] = lines[i].rstrip("\n")
+        row_ct += 1
 
         if lines[i].find('[') >= 0:
             for char in lines[i]:
@@ -20,12 +39,51 @@ def main():
         elif indicator:
             indicator = False
             column_ct = len(lines[i].split())
+            break
+    
+    row_ct -= 1
+    
+    for i in range(column_ct):
+        columns.append([])
+    
+    # Add the characters to the stacks (note they are in reverse order)
+    for i in range(row_ct):
+        for j in range(column_ct):
+            char_loc = (j * 4) + 1
+            character = lines[i][char_loc]
+            # print(character, char_loc)
+            if character != ' ':
+                columns[j].append(character)
+    # print(columns)
 
-    print(crate_ct, column_ct)
+
+    for i in range(column_ct):
+        columns[i].reverse()
+    
+    for line in lines:
+        if line.find("move") >= 0:
+            line.rstrip()
+            line_li = line.split()
+            amount_to_move = int(line_li[1])
+            move_from = int(line_li[3])
+            move_to = int(line_li[5])
+
+            routine(amount_to_move, move_from, move_to)
+            # print(columns)
+
+
+
+    for i in range(len(columns)):
+        output += columns[i].pop()
+            
+    
+
+
+    # print(crate_ct, column_ct, row_ct)
             
 
     
-    print(total)
+    print(output)
 
 
 if __name__ == "__main__":
